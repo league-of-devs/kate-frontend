c4c6bf8f3c1c3d70524405ae857788213c9a9777
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-04-28 23:11:14 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
-import './assets/style/index.sass'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "./assets/style/index.sass";
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
+
 function App() {
   return (
-    <div>
-      <h1>Kate bot</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/product">
+            <Product />
+          </Route>
+          {/* <Route path="/product/:id"> 
+            <Users />
+          </Route> */}
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
