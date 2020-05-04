8637042f43f0fb33ae1b9bcc1faccc4696bce5bf
Davi Rodrigues <developer.davi@gmail.com>
2020-05-04 19:52:35 -0300

    Fixed product component redirecting

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-import Button from './Button'
+import Button from "./Button";
 
 export default function Products(props){
   const history = useHistory();
   const product = props.prod;
 
-    return (
+  return (
     <>
-        <div className="product">
-            <div className="product--image">
-                <img src={product.picture} alt="product"/>
-            </div>
-            <span className="product--title text" >{product.title}</span>
-            <span className="product--price">{new Intl.NumberFormat('pt-Br', { style: 'currency', currency: 'BRL' }).format(product.price)}</span>
-            <Button sup="2" color="secondary" onClick={()=>{history.push('/product')}}>Ver dados</Button>  
+      <div className="product">
+        <div className="product--image">
+          <img src={product.picture} alt="product"/>
         </div>
+        <span className="product--title text" >{product.title}</span>
+        <span className="product--price">{new Intl.NumberFormat("pt-Br", { style: "currency", currency: "BRL" }).format(product.price)}</span>
+        <Button sup="2" color="secondary" onClick={()=>{history.push(`/product/${product.platform}/${product.id}`);}}>Ver dados</Button>  
+      </div>
     </>
-    );
-};
+  );
+}
