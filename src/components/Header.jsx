a609080ab01e1049f30838669b87ae6ddf5bc62f
Davi Rodrigues <davi.coelho@mundiale.com.br>
2020-05-04 14:24:05 -0300

    Added user name to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import Search from "./Search";
+import { useSelector } from "react-redux";
 
-import logoImg from '../assets/img/logo.png'
+import Search from "./Search";
+import logoImg from "../assets/img/logo.png";
 
 export default function Home(props) {
+
+  const user = useSelector(state => state.User);
+
   return (
-    <header  className={props.type === 'auth' ? 'header big' : 'header'} >
+    <header  className={props.type === "auth" ? "header big" : "header"} >
       <div className="container">
         <div className="header--kate">
           <img src={logoImg}alt="logoImg"/>  
         </div>
-        {props.type !== 'auth' && <>
-          {props.type === 'productsPage' && <Search value={props.search} callback={e => props.callback(e)}/> }
+        {props.type !== "auth" && <>
+          {props.type === "productsPage" && <Search value={props.search} callback={e => props.callback(e)}/> }
           <Link to="/config">
-          <div className="header--user">
-            <span>Alex</span>
-            <FontAwesomeIcon className="icon" icon={faUserCircle} size="2x" />
-          </div>
+            <div className="header--user">
+              <span>{user.name.split(" ")[0]}</span>
+              <FontAwesomeIcon className="icon" icon={faUserCircle} size="2x" />
+            </div>
           </Link>
         </>
         
